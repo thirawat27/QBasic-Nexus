925a22e46ef841dea1c4e5e192421e2501610a81
Thirawat  Sinlapasomsak <thunthirawas@gmail.com>
2025-06-09 09:30:02 +0700

    Version 0.0.4 Update

--- compilers/linux/qb64/internal/c/parts/audio/conversion/src/resample.c
+++
@@ -1,6 +0,0 @@
-// TODO: Get rid of this entire conversion directory and it's content. This is just a stub!
-
-int resample_c_dummy_function()
-{
-    return 0;
-}
--- compilers/macos/qb64/internal/c/parts/audio/decode/ogg/src.c
+++
@@ -1,7 +0,0 @@
-#ifndef DEPENDENCY_AUDIO_DECODE_OGG
-//Stubs:
-//(none required)
-#else
-// TODO: Get rid of this entire decode directory and it's content. This is just a stub!
-#endif
-
